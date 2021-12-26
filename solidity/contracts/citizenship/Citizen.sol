2fa30e9e96d4ea95d50492ed551fda3e032cab65
nithronium <osmankuzucu5@gmail.com>
2021-12-26 12:55:05 +0300

    feat: addition of citizen registration & nominations & birth control

--- solidity/contracts/citizenship/Citizen.sol
+++ solidity/contracts/citizenship/Citizen.sol
@@ -11,7 +11,9 @@
 
 pragma solidity ^0.8.0;
 
-contract Citizen {
+import "../access/Roles.sol";
+
+contract Citizen is Roles {
 
     //Could add some more data
     struct CitizenData {
@@ -22,11 +24,27 @@ contract Citizen {
         uint score_;
         bool isJailed_;
         uint penaltyTime_;
+        uint childCount_;
+    }
+
+    struct CitizenshipNominations {
+        address parentOne_;
+        address parentTwo_;
+        bool parentOneSigned_;
+        bool parentTwoSigned_;
     }
 
     uint256 private _citizenCounter;
     mapping(address=>CitizenData) private _citizens;
     mapping(uint256=>address) private _citizenIds;
+
+    mapping(address=>CitizenshipNominations) private _nominations;
+
+    uint private _allowedChildCount;
+
+    event CitizenshipGranted(address user, address parentOne, address parentTwo, uint birthdate);
+    event UserNominated(address user, address nominatedBy, uint nominationDate);
+    event ParentConsented(address child, address parent, uint consentDate);
 
     modifier onlyCitizen(address user) {
         require(_citizens[user].bornAt_ != 0,"Not citizen");
@@ -37,5 +55,47 @@ contract Citizen {
         require(_citizens[user].bornAt_ != 0 && _citizens[user].isJailed_ == false,"Citizen jailed");
         _;
     }
+
+    modifier birthControl(address parent) {
+        require(_citizens[parent].childCount_ <= _allowedChildCount,"Already too many children");
+        _;
+    }
+
+    constructor() {
+        registerRole('DICTATOR', false, true);
+        _allowedChildCount = 5;
+        grantCitizenship(msg.sender);
+    }
+
+    function grantCitizenship(address user) onlyRole('DICTATOR') public returns (bool) {
+        require(!isCitizen(user),"User already a citizen");
+        _citizens[user].id_ = _citizenCounter;
+        _citizens[user].bornAt_ = block.timestamp;
+        _citizens[user].parentOne_ = msg.sender;
+        _citizens[user].parentTwo_ = address(0);
+        _citizens[user].score_ = 100000000000000000000;
+        _citizens[user].isJailed_ = false;
+        _citizens[user].penaltyTime_ = 0;
+
+        _citizenIds[_citizenCounter] = user;
+        _citizenCounter++;
+
+        emit CitizenshipGranted(user,msg.sender,address(0),block.timestamp);
+
+        return true;
+    }
+
+    function nominateCitizen(address user) onlyFreeCitizen(msg.sender) public returns (bool) {
+
+    }
+
+    function consentBirth(address user) birthControl(msg.sender) onlyFreeCitizen(msg.sender) public returns (bool) {
+
+    }
     
+    // INTERNAL FUNCTIONS // 
+    function isCitizen(address user) public view returns (bool) {
+        if(_citizens[user].bornAt_ == 0) { return true; } 
+        return false;
+    }
 }
